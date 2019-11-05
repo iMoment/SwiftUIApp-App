f7377970b931014bccf6228cd61aacf44333006a
Stanley Pan <stanleypan.egre@gmail.com>
2019-11-06 01:28:03 +0900

    Initial Commit

---
+++ SwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIApp
+//
+//  Created by Stanley Pan on 2019/11/06.
+//  Copyright © 2019 Stanley Pan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
