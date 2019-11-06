b85d5be5a24bcedaa651b89d218d89fca5d1adf5
Stanley Pan <stanleypan.egre@gmail.com>
2019-11-07 03:21:46 +0900

    Add Animation between screens

--- SwiftUIApp/HomeView.swift
+++ SwiftUIApp/HomeView.swift
@@ -10,12 +10,24 @@ import SwiftUI
 
 struct HomeView: View {
     @State var show = false
+    @State var showProfile = false
     
     var body: some View {
-        ZStack {
-            Button(action: { self.show.toggle() }) {
-            Text("Open Menu")
-            }
+        ZStack(alignment: .topLeading) {
+            ContentView()
+                .background(Color.white)
+                .cornerRadius(30)
+                .shadow(radius: 20)
+                .animation(.spring(response: 0.45, dampingFraction: 0.885, blendDuration: 0))
+                .offset(y: showProfile ? 40 : UIScreen.main.bounds.height)
+            
+            MenuButton(show: $show)
+                .offset(x: -30, y: showProfile ? 0 : 80)
+                .animation(.spring())
+            
+            MenuRight(show: $showProfile)
+                .offset(x: -16, y: showProfile ? 0 : 88)
+                .animation(.spring())
             
             MenuView(show: $show)
         }
@@ -81,4 +93,51 @@ struct Menu: Identifiable {
     var id = UUID()
     var title: String
     var iconName: String
+}
+
+struct CircleButton: View {
+    var icon = "person.crop.circle"
+    var body: some View {
+        HStack {
+            Image(systemName: icon)
+                .foregroundColor(.black)
+        }
+        .frame(width: 44, height: 44)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(color: Color("buttonShadow"), radius: 10, x: 0, y: 10)
+    }
+}
+
+struct MenuButton: View {
+    @Binding var show: Bool
+    var body: some View {
+        Button(action: { self.show.toggle() }) {
+            HStack {
+                Spacer()
+                Image(systemName: "list.dash")
+                    .foregroundColor(.black)
+            }
+            .padding(.trailing, 20)
+            .frame(width: 90, height: 60)
+            .background(Color.white)
+            .cornerRadius(30)
+            .shadow(color: Color("buttonShadow"), radius: 10, x: 0, y: 10)
+        }
+    }
+}
+
+struct MenuRight: View {
+    @Binding var show: Bool
+    var body: some View {
+        HStack(spacing: 12) {
+            Spacer() //  Added
+            Button(action: { self.show.toggle() }) {
+                CircleButton(icon: "person.crop.circle")
+            }
+            Button(action: { self.show.toggle() }) {
+                CircleButton(icon: "bell")
+            }
+        }
+    }
 }
