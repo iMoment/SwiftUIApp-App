18f6292d01de88bdd314c6b5b9f8c92e6f60b91a
Stanley Pan <stanleypan.egre@gmail.com>
2019-11-07 18:05:45 +0900

    Add UpdateList.swift

--- SwiftUIApp/SceneDelegate.swift
+++ SwiftUIApp/SceneDelegate.swift
@@ -27,7 +27,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: homeList)
+            window.rootViewController = UIHostingController(rootView: homeView)
             self.window = window
             window.makeKeyAndVisible()
         }
